a49a13e5e5b9d1cad52d0930f4dead3da7a68ebf
Rodolfo <rjofiel97@outlook.es>
2022-06-06 21:30:03 +0200

    Changes in angular.json and resolve issues with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 const { guessProductionMode } = require("@ngneat/tailwind");
 process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
-
+const mode = process.env.TAILWIND_MODE ? 'jit':'aot';
 module.exports = {
+  prefix: '',
+  mode: mode,
   purge: {
     enabled: true,
     content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
   },
   darkMode: "class", // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '360px',
+      md: '1024px',
+      lg: '1440px',
+    },
     extend: {},
   },
   variants: {
